67e8258a004d076ddc1b42ead14c59352cc1f04a
stev1988 <stev1988@foxmail.com>
2018-09-20 20:16:44 +0800

    提交代码

---
+++ 2140key.ico.cpp
@@ -0,0 +1,210 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+
+#include <eosio.token/eosio.token.hpp>
+#include "2140key.ico.hpp"
+
+namespace eosico {
+
+#define TOKEN_SYMBOL    S(4,KEY)
+#define PHASE_ONE       1500000000
+
+    void ico::create( account_name issuer,
+                        asset        maximum_supply )
+    {
+        require_auth( _self );
+
+        auto sym = maximum_supply.symbol;
+        eosio_assert( sym.is_valid(), "invalid symbol name" );
+        eosio_assert( maximum_supply.is_valid(), "invalid supply");
+        eosio_assert( maximum_supply.amount > 0, "max-supply must be positive");
+
+        eosio_assert( maximum_supply.symbol == TOKEN_SYMBOL, "only can create KEY symbol");
+
+        stats statstable( _self, sym.name() );
+        auto existing = statstable.find( sym.name() );
+        eosio_assert( existing == statstable.end(), "token with symbol already exists" );
+
+        statstable.emplace( _self, [&]( auto& s ) {
+           s.supply.symbol = maximum_supply.symbol;
+           s.max_supply    = maximum_supply;
+           s.issuer        = issuer;
+        });
+    }
+
+
+    void ico::issue( account_name to, asset quantity, string memo )
+    {
+        auto sym = quantity.symbol;
+        eosio_assert( sym.is_valid(), "invalid symbol name" );
+        eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
+
+        auto sym_name = sym.name();
+        stats statstable( _self, sym_name );
+        auto existing = statstable.find( sym_name );
+        eosio_assert( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
+        const auto& st = *existing;
+
+        require_auth( st.issuer );
+        eosio_assert( quantity.is_valid(), "invalid quantity" );
+        eosio_assert( quantity.amount > 0, "must issue positive quantity" );
+
+        eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+        eosio_assert( quantity.amount <= st.max_supply.amount - st.supply.amount, "quantity exceeds available supply");
+
+        statstable.modify( st, 0, [&]( auto& s ) {
+           s.supply += quantity;
+        });
+
+        add_balance( st.issuer, quantity, st.issuer );
+
+        if( to != st.issuer ) {
+           SEND_INLINE_ACTION( *this, transfer, {st.issuer,N(active)}, {st.issuer, to, quantity, memo} );
+        }
+    }
+
+    void ico::transfer( account_name from,
+                          account_name to,
+                          asset        quantity,
+                          string       memo )
+    {
+        require_auth( from );
+
+        eosio_assert( from != to, "cannot transfer to self" );
+        eosio_assert( is_account( to ), "to account does not exist");
+        auto sym = quantity.symbol.name();
+        stats statstable( _self, sym );
+        const auto& st = statstable.get( sym );
+
+        require_recipient( from );
+        require_recipient( to );
+
+        eosio_assert( quantity.is_valid(), "invalid quantity" );
+        eosio_assert( quantity.amount > 0, "must transfer positive quantity" );
+        eosio_assert( quantity.symbol == st.supply.symbol, "symbol precision mismatch" );
+        eosio_assert( memo.size() <= 256, "memo has more than 256 bytes" );
+
+
+        sub_balance( from, quantity );
+        add_balance( to, quantity, from );
+    }
+
+    void ico::sub_balance( account_name owner, asset value ) {
+       accounts from_acnts( _self, owner );
+
+       const auto& from = from_acnts.get( value.symbol.name(), "no balance object found" );
+       eosio_assert( from.balance.amount >= value.amount, "overdrawn balance" );
+
+        allaccounts _allaccounts(_self, _self);
+        const auto& itr = _allaccounts.get( owner, "no balance object found" );
+        if(from.balance.amount == value.amount){
+            _allaccounts.erase(itr);
+        }else{
+            _allaccounts.modify( itr, owner, [&]( auto& a ) {
+                a.balance -= value;
+            });
+        }
+
+       if( from.balance.amount == value.amount ) {
+          from_acnts.erase( from );
+       } else {
+          from_acnts.modify( from, owner, [&]( auto& a ) {
+              a.balance -= value;
+          });
+       }
+    }
+
+    void ico::add_balance( account_name owner, asset value, account_name ram_payer )
+    {
+       accounts to_acnts( _self, owner );
+       auto to = to_acnts.find( value.symbol.name() );
+
+        allaccounts _allaccounts(_self, _self);
+        auto itr = _allaccounts.find(owner);
+        if(itr == _allaccounts.end()){
+            _allaccounts.emplace( ram_payer, [&]( auto& a ){
+                a.account = owner;
+                a.balance = value;
+            });
+        }else if(itr != _allaccounts.end()){
+            _allaccounts.modify( itr, 0, [&]( auto& a ) {
+                a.balance += value;
+            });
+        }
+
+
+       if( to == to_acnts.end() ) {
+          to_acnts.emplace( ram_payer, [&]( auto& a ){
+            a.balance = value;
+          });
+       } else {
+          to_acnts.modify( to, 0, [&]( auto& a ) {
+            a.balance += value;
+          });
+       }
+    }
+
+    void ico::buykey(account_name to, asset quantity, string memo){
+        eosio_assert(quantity.symbol == S(4, EOS), "symbol must be EOS");
+
+        stats statstable( _self, symbol_type(TOKEN_SYMBOL).name() );
+        auto existing = statstable.find( symbol_type(TOKEN_SYMBOL).name() );
+        eosio_assert( existing != statstable.end(), "token with symbol does not exist, create token before issue" );
+
+        eosio_assert( existing->supply.amount+quantity.amount <= PHASE_ONE, "the key has been sold more than 150000.0000");
+
+        SEND_INLINE_ACTION( *this, issue, {existing->issuer,N(active)}, {to, asset{quantity.amount, TOKEN_SYMBOL}, memo} );
+    }
+} /// namespace eosio
+
+using namespace eosico;
+#include <eosiolib/action.h>
+
+extern "C" {
+
+void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
+//    print( "\nicoapply,", name{receiver},"\n" );
+
+    auto self = receiver;
+    eosico::ico thiscontract( receiver );
+    if( code == N(eosio.token) &&  action == N(transfer) ) {
+        eosio::token::transfer_args tmp = unpack_action_data<eosio::token::transfer_args>();
+
+        if(tmp.to != self){
+            return;
+        }
+
+        string memo = tmp.memo;
+
+        memo.erase(memo.begin(), find_if(memo.begin(), memo.end(), [](int ch) {
+            return !isspace(ch);
+        }));
+        memo.erase(find_if(memo.rbegin(), memo.rend(), [](int ch) {
+            return !isspace(ch);
+        }).base(), memo.end());
+
+        auto separator_pos = memo.find(' ');
+        if (separator_pos == string::npos) {
+            separator_pos = memo.find(':');
+        }
+
+        eosio_assert(separator_pos != string::npos, "Function name and other command must be separated with space or colon");
+
+        string func_name_str = memo.substr(0, separator_pos);
+        if(0 == func_name_str.compare("buykey")){
+            thiscontract.buykey(tmp.from, tmp.quantity,tmp.memo);
+        }
+
+    }
+    else if (code == self  || action == N(onerror) ){
+        switch (action)
+        {
+            EOSIO_API( eosico::ico, (create)(issue)(transfer) )
+        }
+    }
+}
+}
+
+//EOSIO_ABI( eosico::ico, (create)(issue)(transfer) )
---
+++ cmd.sh
@@ -0,0 +1,193 @@
+
+sh /home/root1/work/ET-exchange/etbexchange/bancor.sh
+cleos --wallet-url http://127.0.0.1:4444 --url http://localhost:8000  get table etbexchanger etbexchanger markets
+
+eosiocpp -o /home/root1/work/etbexchange/etbexchange.wast  /home/root1/work/etbexchange/etbexchange.cpp /home/root1/work/etbexchange/exchange_state.cpp
+eosiocpp -g /home/root1/work/etbexchange/etbexchange.api  /home/root1/work/etbexchange/etbexchange.cpp
+cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000 set contract etbexchanger /home/root1/work/etbexchange -p etbexchanger
+cleos --wallet-url http://127.0.0.1:4444 --url http://localhost:8000  push action etbexchanger pause '[]' -p etbexchanger
+cleos --wallet-url http://127.0.0.1:4444 --url http://localhost:8000  get table etbexchanger etbexchanger markets
+
+eosiocpp -o /home/root1/work/eos/contracts/etbexchange/etbexchange.wast  /home/root1/work/eos/contracts/etbexchange/etbexchange.cpp /home/root1/work/eos/contracts/etbexchange/exchange_state.cpp
+eosiocpp -g /home/root1/work/eos/contracts/etbexchange/etbexchange.api  /home/root1/work/eos/contracts/etbexchange/etbexchange.cpp
+cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000 set contract etbexchanger /home/root1/work/eos/contracts/etbexchange -p etbexchanger
+cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000 push action etbexchanger restart '[]' -p etbexchanger 
+cleos --wallet-url http://127.0.0.1:4444 --url http://localhost:8000  get table etbexchanger etbexchanger markets
+
+
+eosiocpp -o /home/root1/work/eos/contracts/2140key.ico/2140key.ico.wast  /home/root1/work/eos/contracts/2140key.ico/2140key.ico.cpp 
+eosiocpp -g /home/root1/work/eos/contracts/2140key.ico/2140key.ico.api  /home/root1/work/eos/contracts/2140key.ico/2140key.ico.cpp
+
+#cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000 set contract  issuerkeyacc /home/root1/work/eos/contracts/2140key.ico -p  issuerkeyacc
+
+
+cleos --wallet-url http://localhost:6666  wallet unlock --password=PW5KY1uq5mBaJRizwHhPAYp1w71yXRgAT322gKR6Yh7Uwx4BymBai
+
+cleos --wallet-url http://localhost:6666  wallet import  --private-key 5KDsFepEAS7hEpnkRVFk7NQiX7gxCTVvUVTTVvi84XSkuNKRaVh
+
+#cleos --wallet-url http://localhost:6666 --url http://localhost:8000 create account eosio eosio.token EOS7nnGJ7Ra911dwR1rQFw2MD2M8RkRPzUBtYb3qBmuYfaxbkUWmd EOS7nnGJ7Ra911dwR1rQFw2MD2M8RkRPzUBtYb3qBmuYfaxbkUWmd
+
+#cleos --wallet-url http://localhost:6666 --url http://localhost:8000 set contract eosio.token /home/root1/work/eos/build/contracts/eosio.token -p eosio.token
+
+#cleos --wallet-url http://localhost:6666 --url http://localhost:8000 push action eosio.token create '["eosio", "1000000.0000 EOS", 0, 0, 0]' -p eosio.token
+
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 push action eosio.token issue '[ "eosio", "100000000.0000 EOS", "memo"]' -p eosio
+
+
+cleos --wallet-url http://127.0.0.1:4444 wallet unlock --password=PW5HzA1Gbp8mCE4grJHnFsSTFWnoynhLLKjnsBHQsRSAdWcuaMmjD
+
+#创建交易所合约账户 etbexchange5
+#Private key: 5HrvtqXNEDSJyjqcoxr45Frgxatf2nMWcNtcV2NqW2zD4dctoWM
+#Public key: EOS6D1nt1aYtWPbfm5qburgRZc8gHVY6pwAcu812U9CnYigJN1saA
+cleos --wallet-url http://127.0.0.1:4444 wallet import  --private-key 5HrvtqXNEDSJyjqcoxr45Frgxatf2nMWcNtcV2NqW2zD4dctoWM
+
+cleos  --wallet-url http://localhost:6666   --url http://localhost:8000 system newaccount --transfer eosio etbexchanger EOS6D1nt1aYtWPbfm5qburgRZc8gHVY6pwAcu812U9CnYigJN1saA --stake-net "1000.0000 EOS" --stake-cpu "1000.0000 EOS" --buy-ram "10000.0000 EOS"
+
+
+cleos --wallet-url http://127.0.0.1:4444 wallet import  --private-key 5HxS1BExGpZkink9wxK9kfhiLsqqvGPwtuqqyCCGn2mCU9Gux2M
+cleos  --wallet-url http://localhost:6666   --url http://localhost:8000 system newaccount --transfer eosio  etbexchange5 EOS7YAQTAALWQ6mRZCNchz3sGivmzzfrdmrY1EDat9jodXBwj6ShN --stake-net "1000.0000 EOS" --stake-cpu "1000.0000 EOS" --buy-ram "100.0000 EOS"
+
+
+
+
+
+#创建交易所资金账户exchangeacc1,用来存放EOS和ETB代币
+#Private key: 5HxS1BExGpZkink9wxK9kfhiLsqqvGPwtuqqyCCGn2mCU9Gux2M
+#Public key: EOS7YAQTAALWQ6mRZCNchz3sGivmzzfrdmrY1EDat9jodXBwj6ShN
+cleos --wallet-url http://127.0.0.1:4444 wallet import  --private-key 5HxS1BExGpZkink9wxK9kfhiLsqqvGPwtuqqyCCGn2mCU9Gux2M
+cleos  --wallet-url http://localhost:6666   --url http://localhost:8000 system newaccount --transfer eosio gameone54321 EOS7YAQTAALWQ6mRZCNchz3sGivmzzfrdmrY1EDat9jodXBwj6ShN --stake-net "1000.0000 EOS" --stake-cpu "1000.0000 EOS" --buy-ram "100.0000 EOS"
+
+
+#Private key: 5Jt1McNGg4YhATfRtuz6N6VVePY8srJTdP15vWNezqjofmDiku3
+#Public key: EOS6HLK29n42rxDdrzFicu5gp2Xfgubwbe29Ue57Uux7APp8bsCtE
+cleos --wallet-url http://127.0.0.1:4444 wallet import  --private-key 5Jt1McNGg4YhATfRtuz6N6VVePY8srJTdP15vWNezqjofmDiku3
+cleos  --wallet-url http://localhost:6666   --url http://localhost:8000 system newaccount --transfer eosio  etbexchange5 EOS6HLK29n42rxDdrzFicu5gp2Xfgubwbe29Ue57Uux7APp8bsCtE --stake-net "1000.0000 EOS" --stake-cpu "1000.0000 EOS" --buy-ram "100.0000 EOS"
+
+
+
+#移除etbissue1111权限
+#cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000  set account permission etbissue1111 owner '{"threshold": 1,"keys": [],"accounts": [{"permission":{"actor":"etbico111111","permission":"eosio.code"},"weight":1}]}'  -p etbissue1111@owner
+
+#cleos  --wallet-url http://127.0.0.1:4444  --url http://localhost:8000 set account permission etbissue1111 active '{"threshold": 1,"keys": [],"accounts": [{"permission":{"actor":"etbico111111","permission":"eosio.code"},"weight":1}]}' owner -p etbissue1111
+
+#创建用户账户
+cleos --wallet-url http://localhost:6666 --url http://localhost:8000  system newaccount --transfer eosio user11111111 EOS7nnGJ7Ra911dwR1rQFw2MD2M8RkRPzUBtYb3qBmuYfaxbkUWmd --stake-net "1000.0000 EOS" --stake-cpu "1000.0000 EOS" --buy-ram "100.0000 EOS"
+
+#Private key: 5Jo6Ceomf55SomY3ywwUHVBXK5GzLZF3UCbAgXKP4fvVotLYP1E
+#Public key: EOS7TbrWEfDYug9DTu3rERagB5f2kYVZrB9bS8VU9kwjT9DWH9s8B
+cleos --wallet-url http://localhost:6666  wallet import  --private-key 5Jo6Ceomf55SomY3ywwUHVBXK5GzLZF3UCbAgXKP4fvVotLYP1E
+cleos --wallet-url http://localhost:6666 --url http://localhost:8000  system newaccount --transfer eosio user22222222 EOS7TbrWEfDYug9DTu3rERagB5f2kYVZrB9bS8VU9kwjT9DWH9s8B --stake-net "1000.0000 EOS" --stake-cpu "1000.0000 EOS" --buy-ram "100.0000 EOS"
+
+
+cleos --wallet-url http://localhost:6666 --url http://localhost:8000  system newaccount --transfer eosio user44444444 EOS7TbrWEfDYug9DTu3rERagB5f2kYVZrB9bS8VU9kwjT9DWH9s8B --stake-net "0000.0000 EOS" --stake-cpu "0000.0000 EOS" --buy-ram "100.0000 EOS"
+
+
+
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 push action eosio.token issue '["user11111111", "100000000.0000 EOS", "memo"]' -p eosio
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 push action eosio.token issue '["user22222222", "100000000.0000 EOS", "memo"]' -p eosio
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 push action eosio.token issue '["user44444444", "100000000.0000 EOS", "memo"]' -p eosio
+
+
+#创建 issuerkeyacc账户，用来发ETB代币
+#Private key: 5KheFqEF87wiipLnDSGovivzNizRdM9qkY49q2SnEFdpfEKrvDK
+#Public key: EOS7hB21d8HzVEwV4EF984J1fjGYJw7CfNhuCAtfpSu729xAtkytt
+
+cleos --wallet-url http://127.0.0.1:4444 wallet import  --private-key 5KheFqEF87wiipLnDSGovivzNizRdM9qkY49q2SnEFdpfEKrvDK
+
+cleos  --wallet-url http://localhost:6666   --url http://localhost:8000 system newaccount --transfer eosio  issuerkeyacc EOS7hB21d8HzVEwV4EF984J1fjGYJw7CfNhuCAtfpSu729xAtkytt --stake-net "1000.0000 EOS" --stake-cpu "1000.0000 EOS" --buy-ram "100.0000 EOS"
+
+
+
+
+cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000 set contract  issuerkeyacc /home/root1/work/eos/contracts/2140key.ico -p  issuerkeyacc
+
+cleos  --wallet-url http://127.0.0.1:4444  --url http://localhost:8000 push action  issuerkeyacc create '["issuerkeyacc", "1000000.0000 KEY", 0, 0, 0]' -p  issuerkeyacc
+
+#cleos  --wallet-url http://localhost:4444 --url http://localhost:8000 push action  issuerkeyacc issue '[" etbexchange5", "850000.0000 KEY", "memo"]' -p  issuerkeyacc
+
+#cleos  --wallet-url http://127.0.0.1:4444  --url http://localhost:8000 push action  issuerkeyacc issue '[ "user11111111", "1000000.0000 ETB", "memo"]' -p  issuerkeyacc
+#cleos  --wallet-url http://127.0.0.1:4444  --url http://localhost:8000 push action  issuerkeyacc issue '[ "user22222222", "1000000.0000 ETB", "memo"]' -p  issuerkeyacc
+#cleos  --wallet-url http://127.0.0.1:4444  --url http://localhost:8000 push action  issuerkeyacc issue '[ "etbexchanger", "100000000.0000 YZB", "memo"]' -p  issuerkeyacc
+
+
+cleos  --wallet-url http://localhost:4444 --url http://localhost:8000  set account permission  issuerkeyacc active '{"threshold": 1,"keys": [{"key": "EOS7hB21d8HzVEwV4EF984J1fjGYJw7CfNhuCAtfpSu729xAtkytt","weight": 1}],"accounts": [{"permission":{"actor":"issuerkeyacc","permission":"eosio.code"},"weight":1}]}' owner -p  issuerkeyacc
+
+
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 get currency balance eosio.token user11111111
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 get currency balance eosio.token user22222222
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 get currency balance eosio.token issuerkeyacc
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 get currency balance eosio.token etbexchanger
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 get currency balance eosio.token etbexchange5
+
+
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 get currency balance  issuerkeyacc user11111111
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 get currency balance  issuerkeyacc user22222222
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 get currency balance  issuerkeyacc etbexchanger
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 get currency balance  issuerkeyacc  etbexchange5
+
+
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 push action eosio.token create '["eosio", "10000000000.0000 BTC", "memo"]' -p eosio.token
+
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 push action eosio.token issue '["user11111111", "100000000.0000 BTC", "memo"]' -p eosio
+
+cleos  --wallet-url http://127.0.0.1:6666 --url http://localhost:8000 push action eosio.token transfer '["user11111111", "issuerkeyacc","150000.0000 BTC","buykey:"]' -p user11111111
+
+
+cleos  --wallet-url http://127.0.0.1:6666 --url http://localhost:8000 push action eosio.token transfer '["user11111111", "issuerkeyacc","150000.0000 EOS","buykey:"]' -p user11111111
+
+cleos  --wallet-url http://127.0.0.1:6666 --url http://localhost:8000 push action eosio.token transfer '["user11111111", "issuerkeyacc","1.0000 EOS","buykey:"]' -p user11111111
+
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000 get currency stats  issuerkeyacc KEY
+
+
+
+
+
+cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000 push action eosio.token transfer '["issuerkeyacc", "etbexchanger","50000.0000 EOS","buykey:"]' -p  issuerkeyacc
+
+cleos  --wallet-url http://localhost:4444 --url http://localhost:8000 push action  issuerkeyacc issue '["etbexchanger", "850000.0000 KEY", "memo"]' -p  issuerkeyacc
+
+cleos  --wallet-url http://localhost:4444 --url http://localhost:8000  set account permission etbexchanger active '{"threshold": 1,"keys": [{"key": "EOS6D1nt1aYtWPbfm5qburgRZc8gHVY6pwAcu812U9CnYigJN1saA","weight": 1}],"accounts": [{"permission":{"actor":"etbexchanger","permission":"eosio.code"},"weight":1}]}' owner -p etbexchanger
+
+cleos  --wallet-url http://localhost:4444 --url http://localhost:8000  set account permission  etbexchange5 active '{"threshold": 1,"keys": [{"key": "EOS7YAQTAALWQ6mRZCNchz3sGivmzzfrdmrY1EDat9jodXBwj6ShN","weight": 1}],"accounts": [{"permission":{"actor":"etbexchanger","permission":"eosio.code"},"weight":1}]}' owner -p  etbexchange5
+
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000  set account permission user11111111 active '{"threshold": 1,"keys": [{"key": "EOS7nnGJ7Ra911dwR1rQFw2MD2M8RkRPzUBtYb3qBmuYfaxbkUWmd","weight": 1}],"accounts": [{"permission":{"actor":"etbexchanger","permission":"eosio.code"},"weight":1}]}' owner -p user11111111
+
+cleos  --wallet-url http://localhost:6666 --url http://localhost:8000  set account permission user22222222 active '{"threshold": 1,"keys": [{"key": "EOS7TbrWEfDYug9DTu3rERagB5f2kYVZrB9bS8VU9kwjT9DWH9s8B","weight": 1}],"accounts": [{"permission":{"actor":"etbexchanger","permission":"eosio.code"},"weight":1}]}' owner -p user22222222
+
+cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000 set contract etbexchanger /home/root1/work/eos/contracts/etbexchange -p etbexchanger
+
+cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000 push action etbexchanger create '["etbexchanger","etbexchange5", "50000.0000 EOS","issuerkeyacc","850000.0000 KEY"]' -p etbexchanger
+
+cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000 push action etbexchanger setparam '["issuerkeyacc","4,KEY","exchange_type","1"]' -p etbexchanger 
+
+cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000 push action etbexchanger setparam '["issuerkeyacc","4,KEY","base_weight","20/1"]' -p etbexchanger
+
+cleos  --wallet-url http://127.0.0.1:4444 --url http://localhost:8000 push action etbexchanger setparam '["issuerkeyacc","4,KEY","quote_weight","1/20"]' -p etbexchanger
+
+cleos --wallet-url http://127.0.0.1:4444 --url http://localhost:8000  get table etbexchanger etbexchanger markets
+exit
+
+cleos --wallet-url http://localhost:6666 --url http://localhost:8000  push action etbexchanger buytoken '["user22222222", "1.0000 EOS","issuerkeyacc", "4,KEY", "feeaccount", "0"]' -p user22222222
+
+cleos --wallet-url http://localhost:6666 --url http://localhost:8000  push action etbexchanger selltoken '["user22222222", "issuerkeyacc", "0.8499 KEY", "feeaccount", "0"]' -p user22222222
+
+cleos --wallet-url http://localhost:6666 --url http://localhost:8000  push action etbexchanger selltoken '["user11111111","issuerkeyacc", "150000.0000 KEY", "feeaccount", "0"]' -p user11111111
+
+cleos --wallet-url http://localhost:6666 --url http://localhost:8000  push action etbexchanger buytoken '["user11111111", "50001.0000 EOS","issuerkeyacc", "4,KEY", "feeaccount", "0"]' -p user11111111
+
+cleos --wallet-url http://127.0.0.1:4444 --url http://localhost:8000  get table etbexchanger etbexchanger markets
+
+cleos --wallet-url http://127.0.0.1:4444 --url http://localhost:8000  get table issuerkeyacc issuerkeyacc allaccounts
+
+
+
+
+
+
+
+
+
+
+
+
